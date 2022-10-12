e29351334f72eabcb0f5b7e2e59485475555ef15
GaryLiao <gary258796@gmail.com>
2022-10-12 14:22:31 +0800

    Update

--- src/main/java/gary/springframework/garycustomstarter/configuration/GaryAutoConfiguration.java
+++ src/main/java/gary/springframework/garycustomstarter/configuration/GaryAutoConfiguration.java
@@ -7,10 +7,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ConditionalOnClass(Greeter.class)
 @EnableConfigurationProperties(GaryProperties.class)
+@PropertySource(value = "classpath:custom.properties")
 public class GaryAutoConfiguration {
 
     @Autowired
